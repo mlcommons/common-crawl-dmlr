1dbeaac39dd98df4af1025f7d3544d50d3737d71
Pedro Ortiz Suarez <pedro@commoncrawl.org>
2024-08-02 02:35:15 +0200

    feat: Added all the json and parquet structures for the serializer and deserializer

--- oscar2parquet/src/main.rs
+++ oscar2parquet/src/main.rs
@@ -3,14 +3,15 @@ use clap::Parser;
 use crate::cli::Commands;
 
 mod cli;
-mod lote;
+mod convert;
+mod oscar;
 
 #[tokio::main]
 async fn main() {
     let args = cli::Cli::parse();
     match args.command.unwrap() {
         Commands::Lote { src, dst } => {
-            lote::extract_lote(&src, &dst).await;
+            convert::convert_to_parquet(&src, &dst).await;
         }
     }
 }
