66fb4b238ce9938738c201c198bff463a77f9d74
Pedro Ortiz Suarez <pedro@commoncrawl.org>
2025-07-06 23:07:46 +0000

    fix: add a field for the alternative script in the builder

--- madlad-sampler/src/schemas.rs
+++ madlad-sampler/src/schemas.rs
@@ -16,6 +16,7 @@ pub struct MadDocument {
 pub struct Document {
     pub text: String,
     pub lang: String,
+    pub script: String,
     pub timestamp: Option<String>,
     pub url: Option<String>,
     pub clean: bool,
@@ -27,6 +28,7 @@ pub struct Document {
 struct MadBuilder {
     text: StringBuilder,
     lang: StringBuilder,
+    script: StringBuilder,
     timestamp: StringBuilder,
     url: StringBuilder,
     clean: BooleanBuilder,
@@ -38,6 +40,7 @@ impl MadBuilder {
     fn append(&mut self, document: &Document) {
         self.text.append_value(document.text.as_str());
         self.lang.append_value(document.lang.as_str());
+        self.script.append_option(document.lang.as_str());
         self.timestamp.append_option(document.timestamp.as_ref());
         self.url.append_option(document.url.as_ref());
         self.clean.append_value(document.clean);
@@ -48,10 +51,13 @@ impl MadBuilder {
     /// Note: returns StructArray to allow nesting within another array if desired
     fn finish(&mut self) -> StructArray {
         let text = Arc::new(self.text.finish()) as ArrayRef;
-        let text_field = Arc::new(Field::new("text", DataType::Utf8, true));
+        let text_field = Arc::new(Field::new("text", DataType::Utf8, false));
 
         let lang = Arc::new(self.lang.finish()) as ArrayRef;
-        let lang_field = Arc::new(Field::new(";ang", DataType::Utf8, true));
+        let lang_field = Arc::new(Field::new(";ang", DataType::Utf8, false));
+
+        let script = Arc::new(self.script.finish()) as ArrayRef;
+        let script_field = Arc::new(Field::new("script", DataType::Utf8, true));
 
         let timestamp = Arc::new(self.timestamp.finish()) as ArrayRef;
         let timestamp_field = Arc::new(Field::new("timestamp", DataType::Utf8, true));
@@ -60,17 +66,18 @@ impl MadBuilder {
         let url_field = Arc::new(Field::new("url", DataType::Utf8, true));
 
         let clean = Arc::new(self.clean.finish()) as ArrayRef;
-        let clean_field = Arc::new(Field::new("clean", DataType::Boolean, true));
+        let clean_field = Arc::new(Field::new("clean", DataType::Boolean, false));
 
         let source = Arc::new(self.source.finish()) as ArrayRef;
         let source_field = Arc::new(Field::new("source", DataType::Utf8, true));
 
         let version = Arc::new(self.version.finish()) as ArrayRef;
-        let version_field = Arc::new(Field::new("version", DataType::Utf8, true));
+        let version_field = Arc::new(Field::new("version", DataType::Utf8, false));
 
         StructArray::from(vec![
             (text_field, text),
             (lang_field, lang),
+            (script_field, script),
             (timestamp_field, timestamp),
             (url_field, url),
             (clean_field, clean),
